3dc3d3e69a790ebf5f27d510734b62d3d79d3864
HoangDuyHuong <dellcook69@gmail.com>
2025-10-24 16:17:49 +0700

    study OOP in SV

---
+++ Inheritance/packet.sv
@@ -0,0 +1,85 @@
+// Inheritance
+// class Packet;
+// 	int status = 1;
+//   function void print();
+//     $display("this's base class");
+//   endfunction
+// endclass
+
+// class subpacket extends Packet;
+// 	int status = 2;
+//   function void print();
+//     $display("this's child class");
+//   endfunction
+  
+//   function int overral_status();
+//   	overral_status = super.status + this.status;
+//   endfunction
+// endclass
+
+
+// virtual Method 
+// class Packet;
+//   bit[3:0] command;
+//   bit[7:0] address;
+  
+//   virtual task creat_pkt();
+//     $display("Packet Payload");
+//   endtask
+  
+//   task sent_pkt;
+//     creat_pkt();
+//   endtask
+// endclass
+
+// class sub_packet extends Packet;
+//   bit[1:0] pid;
+  
+//   virtual task creat_pkt();
+//     $display("subpacket payload");
+//   endtask
+// endclass
+
+
+// Static properties/method
+// class Packet;
+//   bit[3:0] command;
+//   bit[7:0] address;
+//   static bit[5:0] num;
+  
+//   static function void print();
+//     $display("Packet have num is: %0d", num);
+//   endfunction
+// endclass
+
+
+// const keyword  
+// class packet;
+// 	const int max_payload_size;
+  
+//   function new(int size);
+//   	max_payload_size = size;
+//   endfunction
+// endclass
+
+
+// encapsulation  
+// class packet;
+// 	local int pid;
+// 	protected string status;
+//   function new();
+//   endfunction
+  
+//   function void change(int pid, string status);
+//   	this.pid    = pid;
+//     this.status = status;
+//   endfunction
+  
+//   function void show();
+//     $display("pid: %0d, status: %0s", pid, status);
+//   endfunction
+  
+// endclass
+
+
+
---
+++ Inheritance/testbench.sv
@@ -0,0 +1,70 @@
+// Inheritance 
+ module testbench;
+//    Packet     pkt;
+//  	subpacket sub_pkt;
+//    initial begin
+//      sub_pkt = new();
+//      pkt     = new();
+//      $display("status is: %0d", pkt.status);
+//      pkt.print();
+//      #1;
+//      $display("status is: %0d", sub_pkt.status);
+//      sub_pkt.print();
+     
+//      $display("overral status is: %0d", sub_pkt.overral_status);
+//      $finish;
+//    end
+   
+  
+// virtual Method   
+//    sub_packet sub_pkt;
+//    initial begin
+//      sub_pkt = new();
+//      sub_pkt.sent_pkt();
+//      $finish;
+//    end
+   
+   
+// Static properties/method
+//    No need to call new() function
+//    Packet s_pkt1;
+//    Packet s_pkt2;
+   
+//    initial begin
+//      s_pkt1.num = 5;
+//      s_pkt1.print();
+//      s_pkt2.num = 10;
+//      s_pkt2.print();
+//      $finish;
+//    end
+   
+// const keyword   
+// packet pkt;
+//    initial begin
+//      pkt = new(10);
+//      $display("max payload is: %0d", pkt.max_payload_size);
+//      $finish;
+//    end
+   
+// encapsulation   
+// packet pkt;   
+//    initial begin
+//      pkt = new();
+//      pkt.change(10,"good");
+//      pkt.show();
+     
+// //   ERROR situation 
+//      pkt.pid = 1;
+//      pkt.status = "sent";
+//      $display("pid = %0d, status: $0s", pkt.pid, pkt.status);
+//      $finish;
+//    end
+	   
+   
+ endmodule
+
+
+
+
+
+
